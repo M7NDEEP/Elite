f6b1e6ee33dcaaa8f7bb4134b536750c17a47e4f
Darkhorse27 <mandeep7yadav@gmail.com>
2024-03-21 14:33:21 +0530

    loaderdone

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,16 @@
 import "@/styles/globals.css";
-import Lenis from '@studio-freight/lenis'
-import { useEffect } from "react";
+import Lenis from '@studio-freight/lenis';
+import { useEffect, useState, useRef } from "react";
 import gsap from 'gsap';
 import Cursor from "./Cursor";
 import Footer from "@/components/Footer";
 import Contact from "@/components/Contact";
+import Loader from "@/components/Loader";
 
 export default function App({ Component, pageProps }) {
+  const [isContentLoaded, setIsContentLoaded] = useState(false);
+  const [showLoader, setShowLoader] = useState(true);
+  const counterDelay = 100;
 
   useEffect(() => {
     const lenis = new Lenis();
@@ -18,11 +22,25 @@ export default function App({ Component, pageProps }) {
   }, []);
 
   useEffect(() => {
+    const handleLoad = () => {
+      setIsContentLoaded(true);
+    };
+
+    const loaderTimeout = setTimeout(() => {
+      setShowLoader(false);
+    }, 10000);
+
+    return () => {
+      clearTimeout(loaderTimeout);
+    };
   }, []);
 
-  return <>
-    <Cursor />
-    <Component {...pageProps} />
-    <Footer />
-  </>;
-}
+  return (
+    <>
+      {showLoader && <Loader counterDelay={counterDelay} />}
+      <Cursor />
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  );
+};
