898899049569f2909666046b46d87a2c3bd107dd
Darkhorse27 <mandeep7yadav@gmail.com>
2024-03-21 23:23:13 +0530

    contentleft

--- src/pages/events.js
+++ src/pages/events.js
@@ -6,6 +6,7 @@ import Link from "next/link";
 import { FaLocationDot } from "react-icons/fa6";
 import { MdDateRange } from "react-icons/md";
 import { FaExternalLinkAlt } from "react-icons/fa";
+import { RxHamburgerMenu } from "react-icons/rx";
 
 const events = () => {
   const upcomingevent = { name: "wittyhacks", location: "Indore, India", description: 'Wittyhacks prides itself on being Central India’s biggest community hackathon. Since its inception in 2018, Wittyhacks has been a platform where technology leaders and the brightest minds come together to collaborate on building tools that solve real problems.', date: '5 April 2024', link: '', image: 'https://wittyhacks4.devfolio.co/_next/image?url=https%3A%2F%2Fassets.devfolio.co%2Fhackathons%2Fe9e2409ea9734a56bedc7dc688c8ff3c%2Fassets%2Fcover%2F918.jpeg&w=1440&q=100'};
@@ -39,7 +40,7 @@ const events = () => {
 
         <div className={styles.page3}>
         <h3>Past Events</h3> <br />
-        <div style={{display:'flex',justifyContent:'center'}}>
+        <div className={styles.page3past}>
         <img src={upcomingevent.image} alt="no_image_found" />
           <div className={styles.pastC}>
             <Link href={upcomingevent.link}><h1>{upcomingevent.name} <FaExternalLinkAlt size={20} style={{marginLeft:'0.5rem'}}/></h1></Link>
